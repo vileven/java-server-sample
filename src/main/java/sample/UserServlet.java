72825579314b6324a765189659cda6ad5a8b8814
vileven <vileven11@icloud.com>
2017-02-17 09:41:29 +0300

    reflection

--- src/main/java/sample/UserServlet.java
+++ src/main/java/sample/UserServlet.java
@@ -7,27 +7,35 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class UserServlet extends HttpServlet {
 
     private UserHandler handler;
 
-
-    /**
-     * Получаем в качестве аргументов java-представления запроса и ответа.
-     * Из особенностей можно отметить только метод  HttpSession getSession() в {@link HttpServletRequest}
-     * Возвращает объект, который связан с текущим сеансом связи пользователя с сервером.
-     * Идентификация производится по куке JSESSIONID,
-     * которую автоматически вешает jetty. С помощью этого объекта можно
-     * сохранять данные между запросами (см. setAttribute())
-     *
-     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if (handler == null) {
-            handler = new UserHandler();
+            try {
+                //Если бы у UserHandler конструктор зависил бы от каких-то аргументов, мы бы могли
+                //получить их с помощью Constructor.getParameters() и перед вызовом найти или сконструировать уже их.
+                //Это называется dependency injection или, коротко, DI
+                final Constructor<UserHandler> constructor = UserHandler.class.getConstructor();
+                System.out.println(Arrays.toString(constructor.getParameters()));
+
+                handler = constructor.newInstance();
+
+            } catch (ReflectiveOperationException e) {
+                throw new RuntimeException(e);
+            }
         }
         final String msg = req.getParameter("msg");
-        handler.handle(msg, resp);
+        try {
+            //Этот метод мы можем найти не только по имени, но и, например, через аннотацию. И здесь так же возможно использовать DI
+            final Method handle = UserHandler.class.getMethod("handle", String.class, HttpServletResponse.class);
+            handle.invoke(handler, msg, resp);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
